20a06b81f8e40d7de922e053a2d6c74c8c29b7b4
kisyam <how2logout@gmail.com>
2023-03-27 21:50:59 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeword(event.target.value);
+    console.log("I run all the time");
+    useEffect(() => {
+        console.log("Call The API....");
+    }, []);
+    useEffect(() => {
+        console.log("Search for", keyword);
+    }, [keyword]);
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!!</h1>
-            <Button text={"Continue"} />
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here...."
+            />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>Click me</button>
         </div>
     );
 }
